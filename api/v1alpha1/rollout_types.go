5da0b91e47786edd9e732576d32fdd77522ef645
liheng.zms <liheng.zms@alibaba-inc.com>
2022-02-21 15:03:53 +0800

    rollout apis

--- api/v1alpha1/rollout_types.go
+++ api/v1alpha1/rollout_types.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2022 Kruise Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -205,10 +202,10 @@ const (
 	ProgressingReasonPaused       = "Paused"
 
 	// Terminating condition
-	RolloutConditionTerminating RolloutConditionType = "Terminating"
+	RolloutConditionTerminating    RolloutConditionType = "Terminating"
 	// Terminating Reason
-	TerminatingReasonInTerminating = "InTerminating"
-	TerminatingReasonCompleted     = "Completed"
+	TerminatingReasonInTerminating                      = "InTerminating"
+	TerminatingReasonCompleted                          = "Completed"
 )
 
 // CanaryStatus status fields that only pertain to the canary rollout
@@ -221,7 +218,7 @@ type CanaryStatus struct {
 	// +optional
 	CanaryRevision string `json:"canaryRevision"`
 	// CanaryReplicas the numbers of canary revision pods
-	CanaryReplicas int32 `json:"canaryReplicas"`
+	CanaryReplicas      int32 `json:"canaryReplicas"`
 	// CanaryReadyReplicas the numbers of ready canary revision pods
 	CanaryReadyReplicas int32 `json:"canaryReadyReplicas"`
 	// CurrentStepIndex defines the current step of the rollout is on. If the current step index is null, the
