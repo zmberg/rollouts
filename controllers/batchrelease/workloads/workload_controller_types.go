9ba10cf4263a0ea53439dc730d8868c6ea399b2c
veophi <vec.g.sun@gmail.com>
2022-02-22 19:40:24 +0800

    .
    Signed-off-by: veophi <vec.g.sun@gmail.com>

--- controllers/batchrelease/workloads/workload_controller_types.go
+++ controllers/batchrelease/workloads/workload_controller_types.go
@@ -36,38 +36,42 @@ const (
 
 // WorkloadController is the interface that all type of cloneSet controller implements
 type WorkloadController interface {
-	// IfNeedToProgress makes sure that the resources can be upgraded according to the rollout plan
-	// it returns if the verification succeeded/failed or should retry
+	// IfNeedToProgress makes sure that the resources can be upgraded according to the release plan.
+	// it returns 'true' if the verification is succeeded.
+	// it returns 'false' if the verification should retry.
+	// it returns not-empty error if the verification has something wrong, and should not retry.
 	IfNeedToProgress() (bool, error)
 
-	// Prepare make sure that the resource is ready to be upgraded
-	// this function is tasked to do any initialization work on the resources
-	// it returns if the initialization succeeded/failed or should retry
-	Prepare() (bool, error)
+	// PrepareBeforeProgress make sure that the resource is ready to be progressed.
+	// this function is tasked to do any initialization work on the resources.
+	// it returns 'true' if the verification is succeeded.
+	// it returns 'false' if the verification should retry.
+	// it returns not-empty error if the verification has something wrong, and should not retry.
+	PrepareBeforeProgress() (bool, error)
 
-	// RolloutOneBatchPods tries to upgrade pods in the resources following the rollout plan
+	// ProgressOneBatchReplicas tries to upgrade pods in the resources following the rollout plan
 	// it will upgrade pods as the rollout plan allows at once
 	// it returns if the upgrade actionable items succeeded/failed or should continue
-	RolloutOneBatchPods() (bool, error)
+	ProgressOneBatchReplicas() (bool, error)
 
-	// CheckOneBatchPods checks how many pods are ready to serve requests in the current batch
+	// CheckOneBatchReplicas checks how many pods are ready to serve requests in the current batch
 	// it returns whether the number of pods upgraded in this round satisfies the rollout plan
-	CheckOneBatchPods() (bool, error)
+	CheckOneBatchReplicas() (bool, error)
 
 	// FinalizeOneBatch makes sure that the rollout can start the next batch
 	// it returns if the finalization of this batch succeeded/failed or should retry
 	FinalizeOneBatch() (bool, error)
 
-	// Finalize makes sure the resources are in a good final state.
+	// FinalizeProgress makes sure the resources are in a good final state.
 	// It might depend on if the rollout succeeded or not.
 	// For example, we may remove the source object to prevent scalar traits to ever work
 	// and the finalize rollout web hooks will be called after this call succeeds
-	Finalize(pause *bool, cleanup bool) bool
+	FinalizeProgress(pause *bool, cleanup bool) bool
 
-	// WatchWorkload will observe and compare the status recorded in release.status and the real-time
+	// SyncWorkload will watch and compare the status recorded in release.status and the real-time
 	// workload status. If workload status is inconsistent with that recorded in release.status,
 	// will return the corresponding WorkloadChangeEventType and info.
-	WatchWorkload() (WorkloadChangeEventType, *WorkloadAccessor, error)
+	SyncWorkload() (WorkloadChangeEventType, *WorkloadAccessor, error)
 }
 
 type Status struct {
